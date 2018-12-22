811b63f37f4a486f02589cd31707228672ebc2e1
hrdlifil <filip.hrdlicka@gmail.com>
2018-12-22 23:28:20 +0100

    first commit

---
+++ autoklice.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filip
+ * Date: 22. 12. 2018
+ * Time: 23:16
+ */
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+        *{
+            box-sizing: border-box;
+            padding: 0;
+            margin: 0;
+        }
+        html {
+            height: 100%;
+            background: url("pozadi.svg") no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: contain;
+        }
+        body{
+
+            height: 100%;
+            padding: 0;
+            margin: 0;
+
+        }
+
+        img {
+            /* Set rules to fill background */
+            min-height: 100%;
+            min-width: 1024px;
+
+            /* Set up proportionate scaling */
+            width: 100%;
+            height: auto;
+
+            /* Set up positioning */
+            position: fixed;
+            top: 0;
+            left: 0;
+        }
+    </style>
+</head>
+<body>
+
+<img src="jpg.jpg">
+
+</body>
+</html>
