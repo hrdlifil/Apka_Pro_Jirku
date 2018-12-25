6c4f1c62b4b45cc339956c1737041418fa181337
hrdlifil <filip.hrdlicka@gmail.com>
2018-12-25 20:24:48 +0100

    asi nepujde na mobil

--- index.js
+++ index.js
@@ -1,72 +1,114 @@
-class Better_navigation
+class Better_Navigation
 {
     constructor()
     {
     }
 
-    biggerfyPicture1(event)
+    logo1_clicked(event)
     {
         let biggerfiedPicture = document.querySelector("#biggerfied-picture");
-        biggerfiedPicture.style.backgroundImage = "url('walrus2.jpg')";
-        biggerfiedPicture.style.backgroundSize = "cover";
+        //biggerfiedPicture.style.backgroundSize = "cover";
         document.body.classList.add('biggerfied-picture-visible');
     }
 
-    biggerfyPicture2(event){
+    logo2_clicked(event)
+    {
         let biggerfiedPicture = document.querySelector("#biggerfied-picture");
-        biggerfiedPicture.style.backgroundImage = "url('walrus3.jpg')";
-        biggerfiedPicture.style.backgroundSize = "cover";
         document.body.classList.add('biggerfied-picture-visible');
     }
 
-    biggerfyPicture3(event){
+    logo3_clicked(event){
         let biggerfiedPicture = document.querySelector("#biggerfied-picture");
-        biggerfiedPicture.style.backgroundImage = "url('walrus2.jpg')";
-        biggerfiedPicture.style.backgroundSize = "cover";
         document.body.classList.add('biggerfied-picture-visible');
     }
 
-    biggerfyPicture4(event){
+    logo4_clicked(event)
+    {
         let biggerfiedPicture = document.querySelector("#biggerfied-picture");
-        biggerfiedPicture.style.backgroundImage = "url('walrus3.jpg')";
-        biggerfiedPicture.style.backgroundSize = "cover";
         document.body.classList.add('biggerfied-picture-visible');
     }
 
-    biggerfyPicture5(event){
+    logo5_clicked(event)
+    {
         let biggerfiedPicture = document.querySelector("#biggerfied-picture");
-        biggerfiedPicture.style.backgroundImage = "url('walrus2.jpg')";
-        biggerfiedPicture.style.backgroundSize = "cover";
         document.body.classList.add('biggerfied-picture-visible');
     }
 
-    biggerfyPicture6(event){
+    logo6_clicked(event)
+    {
         let biggerfiedPicture = document.querySelector("#biggerfied-picture");
-        biggerfiedPicture.style.backgroundImage = "url('walrus3.jpg')";
-        biggerfiedPicture.style.backgroundSize = "cover";
         document.body.classList.add('biggerfied-picture-visible');
     }
 
-    debiggerfyPicture(event){
+  /*  debiggerfyPicture(event)
+    {
         document.body.classList.remove('biggerfied-picture-visible');
+    }*/
+
+    give_event_listeners()
+    {
+        document.querySelector("#logo1").addEventListener("click", this.logo1_clicked);
+        document.querySelector("#logo2").addEventListener("click", this.logo2_clicked);
+        document.querySelector("#logo3").addEventListener("click", this.logo3_clicked);
+        document.querySelector("#logo4").addEventListener("click", this.logo4_clicked);
+        document.querySelector("#logo5").addEventListener("click", this.logo5_clicked);
+        document.querySelector("#logo6").addEventListener("click", this.logo6_clicked);
+        document.querySelector("#cross").addEventListener("click", this.debiggerfy_picture);
     }
 
-    giveEventListeners(){
-        document.querySelector("#enlarge-btn1").addEventListener("click", this.biggerfyPicture1);
-        document.querySelector("#enlarge-btn2").addEventListener("click", this.biggerfyPicture2);
-        document.querySelector("#enlarge-btn3").addEventListener("click", this.biggerfyPicture3);
-        document.querySelector("#enlarge-btn4").addEventListener("click", this.biggerfyPicture4);
-        document.querySelector("#enlarge-btn5").addEventListener("click", this.biggerfyPicture5);
-        document.querySelector("#enlarge-btn6").addEventListener("click", this.biggerfyPicture6);
-        document.querySelector("#cross").addEventListener("click", this.debiggerfyPicture);
+    transform_into_better_navigation()
+    {
+        let logo1 = document.querySelector("#logo1");
+        let element = document.querySelector("#eshop-odkaz");
+        let element_parent = element.parentNode;
+        element_parent.removeChild(element);
+        element_parent.appendChild(logo1);
+
+        let logo2 = document.querySelector("#logo2");
+        let element2 = document.querySelector("#ztracene_klice_odkaz");
+        let element_parent2 = element2.parentNode;
+        element_parent2.removeChild(element2);
+        element_parent2.appendChild(logo2);
+
+        let logo3 = document.querySelector("#logo3");
+        let element3 = document.querySelector("#otevirani_objektu_a_domu_odkaz");
+        let element_parent3 = element3.parentNode;
+        element_parent3.removeChild(element3);
+        element_parent3.appendChild(logo3);
+
+        let logo5 = document.querySelector("#logo5");
+        let element5 = document.querySelector("#autoklice_odkaz");
+        let element_parent5 = element5.parentNode;
+        element_parent5.removeChild(element5);
+        element_parent5.appendChild(logo5);
+
+        let logo4 = document.querySelector("#logo4");
+        let element4 = document.querySelector("#cenik_odkaz");
+        console.log(element4);
+        let element_parent4 = element4.parentNode;
+        element_parent4.removeChild(element4);
+        element_parent4.appendChild(logo4);
+
+        let logo6 = document.querySelector("#logo6");
+        let element6 = document.querySelector("#otevirani_vozidel_odkaz");
+        let element_parent6 = element6.parentNode;
+        console.log(element_parent6);
+        element_parent6.removeChild(element6);
+        element_parent6.appendChild(logo6);
+    }
+
+    debiggerfy_picture(event)
+    {
+        document.body.classList.remove('biggerfied-picture-visible');
     }
 }
 
 // vytvori objekt ktery zlepsi navigaci tak, ze nebudu muset klikat primo na odkaz a ale staci mi kliknout na li
 
 
-let better_navigation = new Better_navigation();
-galeryLogic.giveEventListeners();
+let better_navigation = new Better_Navigation();
+better_navigation.give_event_listeners();
+better_navigation.transform_into_better_navigation();
 
 
 
--- index.php
+++ index.php
@@ -5,38 +5,45 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hlavni strana</title>
     <link rel="stylesheet" href="index.css" />
 </head>
 <body>
 <div class="contain">
     <div class="header">
-        <img id="zluta" src="images/zluta.png"><img id="logo" src="images/samonil.svg"><img id="telefon" src="images/telefon.svg"><img id="pruhy_nahore" src="images/pruhy.png">
+        <img id="zluta" src="images/zluta.png"/><img id="pruhy_nahore" src="images/pruhy.png"/>
+        <img id="logo" src="images/samonil.svg"/>
+        <img id="telefon" src="images/telefon.svg"/>
+
     </div>
     <div class="middle">
         <div id="upper_row">
-            <a href="e-shop.php">
+            <a id="eshop-odkaz" href="e-shop.php">
                 <img class="loga" id="logo1" src="images/logo1.png">
             </a>
-           <a href="ztracene_klice.php">
+           <a id="ztracene_klice_odkaz" href="ztracene_klice.php">
                <img class="loga" id="logo2" src="images/logo2.png">
            </a>
-            <a href="otevirani_objektu_a_domu.php">
+            <a id="otevirani_objektu_a_domu_odkaz" href="otevirani_objektu_a_domu.php">
                 <img class="loga" id="logo3" src="images/logo3.png">
             </a>
         </div>
-        <div id="biggerfied-picture">
 
+        <div id="biggerfied-picture">
+            <a id="cross"> &#8855;</a>
         </div>
+
+
         <div id="lower_row">
-            <a href="cenik.php">
-                <img class="loga" id="logo4" src="images/logo4.png">
+            <a id="cenik_odkaz" href="cenik.php">
+                <img class="loga" id="logo4" src="images/logo4.png"/>
             </a>
-            <a href="autoklice.php">
-            <img class="loga" id="logo5" src="images/logo5.png">
+            <a id="autoklice_odkaz" href="autoklice.php">
+            <img class="loga" id="logo5" src="images/logo5.png"/>
             </a>
-            <a href="otevirani_vozidel.php">
-            <img class="loga" id="logo5" src="images/logo6.png">
+            <a id="otevirani_vozidel_odkaz" href="otevirani_vozidel.php">
+            <img class="loga" id="logo6" src="images/logo6.png"/>
             </a>
         </div>
     </div>
