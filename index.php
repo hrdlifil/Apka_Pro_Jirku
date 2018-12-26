fc11c14bc7053d73bb775d7aa6140d36cdfcb3c0
hrdlifil <filip.hrdlicka@gmail.com>
2018-12-26 02:18:09 +0100

    komplet responzivita (snad)

--- index.php
+++ index.php
@@ -7,50 +7,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hlavni strana</title>
-    <link rel="stylesheet" href="index.css" />
+    <link rel="stylesheet" href="index_mobile.css" />
 </head>
 <body>
 <div class="contain">
     <div class="header">
-        <img id="zluta" src="images/zluta.png"/><img id="pruhy_nahore" src="images/pruhy.png"/>
+        <img id="zluta" src="images/zluta.png"/>
+        <img id="pruhy_nahore" src="images/pruhy.png"/>
         <img id="logo" src="images/samonil.svg"/>
         <img id="telefon" src="images/telefon.svg"/>
 
     </div>
     <div class="middle">
-        <div id="upper_row">
-            <a id="eshop-odkaz" href="e-shop.php">
-                <img class="loga" id="logo1" src="images/logo1.png">
-            </a>
-           <a id="ztracene_klice_odkaz" href="ztracene_klice.php">
-               <img class="loga" id="logo2" src="images/logo2.png">
-           </a>
-            <a id="otevirani_objektu_a_domu_odkaz" href="otevirani_objektu_a_domu.php">
-                <img class="loga" id="logo3" src="images/logo3.png">
-            </a>
-        </div>
+        <a id="eshop-odkaz" href="e-shop.php">
+            <img class="loga" id="logo1" src="images/logo1.png">
+        </a>
+        <a id="ztracene_klice_odkaz" href="ztracene_klice.php">
+            <img class="loga" id="logo2" src="images/logo2.png">
+        </a>
+        <a id="otevirani_objektu_a_domu_odkaz" href="otevirani_objektu_a_domu.php">
+            <img class="loga" id="logo3" src="images/logo3.png">
+        </a>
 
         <div id="biggerfied-picture">
             <a id="cross"> &#8855;</a>
         </div>
 
-
-        <div id="lower_row">
-            <a id="cenik_odkaz" href="cenik.php">
-                <img class="loga" id="logo4" src="images/logo4.png"/>
-            </a>
-            <a id="autoklice_odkaz" href="autoklice.php">
+        <a id="cenik_odkaz" href="cenik.php">
+            <img class="loga" id="logo4" src="images/logo4.png"/>
+        </a>
+        <a id="autoklice_odkaz" href="autoklice.php">
             <img class="loga" id="logo5" src="images/logo5.png"/>
-            </a>
-            <a id="otevirani_vozidel_odkaz" href="otevirani_vozidel.php">
+        </a>
+        <a id="otevirani_vozidel_odkaz" href="otevirani_vozidel.php">
             <img class="loga" id="logo6" src="images/logo6.png"/>
-            </a>
-        </div>
+        </a>
     </div>
     <div class="footer">
         <img id="pruhy" src="images/pruhy.png">
     </div>
 </div>
 </body>
-<script src="index.js"></script>
+<script src="index_mobile.js"></script>
 </html>
---
+++ index_mobile.php
@@ -0,0 +1,52 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hlavni strana</title>
+    <link rel="stylesheet" href="index_mobile.css" />
+</head>
+<body>
+<div class="contain">
+    <div class="header">
+        <img id="zluta" src="images/zluta.png"/>
+        <img id="pruhy_nahore" src="images/pruhy.png"/>
+        <img id="logo" src="images/samonil.svg"/>
+        <img id="telefon" src="images/telefon.svg"/>
+
+    </div>
+    <div class="middle">
+            <a id="eshop-odkaz" href="e-shop.php">
+                <img class="loga" id="logo1" src="images/logo1.png">
+            </a>
+            <a id="ztracene_klice_odkaz" href="ztracene_klice.php">
+                <img class="loga" id="logo2" src="images/logo2.png">
+            </a>
+            <a id="otevirani_objektu_a_domu_odkaz" href="otevirani_objektu_a_domu.php">
+                <img class="loga" id="logo3" src="images/logo3.png">
+            </a>
+
+        <div id="biggerfied-picture">
+            <a id="cross"> &#8855;</a>
+        </div>
+
+            <a id="cenik_odkaz" href="cenik.php">
+                <img class="loga" id="logo4" src="images/logo4.png"/>
+            </a>
+            <a id="autoklice_odkaz" href="autoklice.php">
+                <img class="loga" id="logo5" src="images/logo5.png"/>
+            </a>
+            <a id="otevirani_vozidel_odkaz" href="otevirani_vozidel.php">
+                <img class="loga" id="logo6" src="images/logo6.png"/>
+            </a>
+    </div>
+    <div class="footer">
+        <img id="pruhy" src="images/pruhy.png">
+    </div>
+</div>
+</body>
+<script src="index_mobile.js"></script>
+</html>
